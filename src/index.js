f070cd7c277a08b24aa4208e509fb4049448d7db
Jaemin Lee <ljmsp83@naver.com>
2026-03-20 11:04:05 +0900

    자동 배포 테스트

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export default {
 		const htmlHeader = { headers: { "Content-Type": "text/html; charset=utf-8" } };// 1. 홈 경로 (/)
 		const render = Pages[pageName];
 
-		let data = { title: "기본 타이틀", content: "기본 내용" };
+		let data = { title: "기본 타이틀", content: pageName };
 
 		if(render) {
 			return new Response(render(data), htmlHeader);
